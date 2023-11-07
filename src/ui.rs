dc83663a65e1d97dbdc1249d249de3b88e7fb945
Changqing Wang <wang.ch@wehi.edu.au>
2023-11-07 18:34:42 +1100

    add tui_textarea

--- src/ui.rs
+++ src/ui.rs
@@ -1,16 +1,24 @@
 use ratatui::{
-    prelude::{Frame},
+    prelude::{Constraint, Direction, Frame, Layout},
     widgets::{Block, Borders, Paragraph, Wrap},
 };
-
+use tui_textarea::TextArea;
 
 use crate::app::App;
 
 pub fn render(app: &mut App, frame: &mut Frame) {
+    let layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(vec![Constraint::Percentage(80), Constraint::Percentage(20)])
+        .split(frame.size());
+
     frame.render_widget(
         Paragraph::new(app.update())
             .block(Block::default().borders(Borders::ALL))
             .wrap(Wrap { trim: false }),
-        frame.size()
+        layout[0],
     );
+
+    let mut textarea = TextArea::default();
+    frame.render_widget(textarea.widget(), layout[1]);
 }
