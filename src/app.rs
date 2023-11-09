b18681a392f929ffd2703c2ac2b092ce2e75a38e
Changqing Wang <wang.ch@wehi.edu.au>
2023-11-09 11:01:14 +1100

    wip: use intervallum

--- src/app.rs
+++ src/app.rs
@@ -1,21 +1,21 @@
 use crate::read_stylizing::{highligh_matches, merge_intervals};
 use bio::alignment::Alignment;
 use bio::io::fastq;
-use bio::io::fastq::Reader;
 use bio::io::fastq::FastqRead;
+use bio::io::fastq::Reader;
 use bio::pattern_matching::myers::Myers;
 use ratatui::prelude::Line;
 
 #[derive(Debug)]
-pub struct App {
+pub struct App<'a> {
     pub quit: bool,
-    pub search_patterns: Vec<(String, String)>,
+    pub search_patterns: Vec<(String, &'a str)>,
     pub records_buf: Vec<fastq::Record>,
     file: String,
-    reader: Reader<std::io::BufReader<std::fs::File>> // buf_size
+    reader: Reader<std::io::BufReader<std::fs::File>>, // buf_size
 }
 
-impl App {
+impl App<'_> {
     pub fn new(file: String) -> Self {
         let mut reader = fastq::Reader::from_file(file.clone()).expect("Failed to open fastq file");
         let mut record = fastq::Record::new();
@@ -30,7 +30,11 @@ impl App {
         App {
             reader: reader,
             quit: false,
-            search_patterns: vec![("AGATCGGAAGAGCGTCGTGTAGAA".to_string(), "00FF00".to_string())],
+            search_patterns: vec![
+                ("AGATCGGAAGAGCGTCGTGTAGAA".to_string(), "#00FF00"),
+                ("AACGCAGAGGAA".to_string(), "#FF0000"),
+                ("TCTTCCGA".to_string(), "#ffff00"),
+            ],
             records_buf: records,
             file: file,
         }
@@ -41,26 +45,31 @@ impl App {
         self.quit = true;
     }
 
-    pub fn set_search_patterns(&mut self, search_patterns: Vec<(String, String)>) {
-        self.search_patterns = search_patterns;
-    }
+    // TODO: fix lifetime
+    // pub fn set_search_patterns(&mut self, search_patterns: Vec<(String, &str)>) {
+    //     self.search_patterns = search_patterns;
+    // }
 
     pub fn update(&self) -> Vec<Line> {
         let mut result: Vec<Line> = Vec::new();
-        let record = &self.records_buf[0];
-        let seq = String::from_utf8_lossy(record.seq()).to_string();
+        for record in &self.records_buf {
+            let seq = String::from_utf8_lossy(record.seq()).to_string();
 
-        let mut myers = Myers::<u64>::new(b"AGATCGGAAGAGCGTCGTGTAGAA");
-        let _aln = Alignment::default();
-        let matches: Vec<(usize, usize)> = merge_intervals(
-            myers
-                .find_all(record.seq(), 3)
-                .map(|(a, b, _)| (a, b))
-                .collect(),
-        );
-
-        result.push(record.id().to_string().into());
-        result.push(highligh_matches(matches, seq));
+            let mut matches: Vec<(Vec<(usize, usize)>, &str)> = Vec::new();
+            for (pattern, col) in &self.search_patterns {
+                let mut myers_1 = Myers::<u64>::new(pattern.clone().into_bytes());
+                let _aln = Alignment::default();
+                let matches_1: Vec<(usize, usize)> = merge_intervals(
+                    &myers_1
+                        .find_all(record.seq(), 2)
+                        .map(|(a, b, _)| (a, b))
+                        .collect::<Vec<(usize, usize)>>(),
+                );
+                matches.push((matches_1, col));
+            }
+            result.push(record.id().to_string().into());
+            result.push(highligh_matches(&matches, seq, "#0000FF"));
+        }
         result
     }
 }
--- src/read_stylizing.rs
+++ src/read_stylizing.rs
@@ -1,8 +1,41 @@
-use ratatui::prelude::{Line, Span, Style, Stylize};
+use ratatui::prelude::{Line, Span, Style, Stylize, Color};
+use std::str::FromStr;
+use interval::interval_set::*;
+use interval::ops::*;
+use gcollections::ops::set::*;
+use gcollections::ops::Bounded;
 
 
-pub fn merge_intervals(mut intervals: Vec<(usize, usize)>) -> Vec<(usize, usize)> {
+pub fn find_intersections(intervals: &[(usize, usize)]) -> Vec<(usize, usize)> {
+    let mut intersections: Vec<(usize, usize)> = Vec::new();
+
+    for i in 0..intervals.len() {
+        for j in i + 1..intervals.len() {
+            let (start1, end1) = intervals[i];
+            let (start2, end2) = intervals[j];
+
+            // Check for overlap
+            if end1 >= start2 && end2 >= start1 {
+                // Calculate intersection
+                let intersection_start = std::cmp::max(start1, start2);
+                let intersection_end = std::cmp::min(end1, end2);
+                intersections.push((intersection_start, intersection_end));
+            }
+        }
+    }
+
+    merge_intervals(&intersections)
+}
+
+#[test]
+fn test_find_intersections() {
+  let x: Vec<(usize, usize)> = vec![(1,3), (2,4), (5,8), (9,10), (9,10), (9,10)];
+  assert_eq!(find_intersections(&x), vec![(2,3), (9,10)]);
+}
+
+pub fn merge_intervals(intervals: &[(usize, usize)]) -> Vec<(usize, usize)> {
     // Sort intervals based on the start value
+    let mut intervals = intervals.to_vec();
     intervals.sort_by(|a, b| a.0.cmp(&b.0));
     let mut merged: Vec<(usize, usize)> = Vec::new();
     for interval in intervals {
@@ -22,18 +55,91 @@ pub fn merge_intervals(mut intervals: Vec<(usize, usize)>) -> Vec<(usize, usize)
     merged
 }
 
-pub fn highligh_matches(intervals: Vec<(usize, usize)>, input_string: String) -> Line<'static> {
+#[test]
+fn test_merge_intervals() {
+  let x: Vec<(usize, usize)> = vec![(1,3), (2,4), (5,8), (9,10), (9,10), (9,10)];
+  assert_eq!(merge_intervals(&x), vec![(1,4), (5, 8), (9,10)]);
+}
+
+
+fn find_overlap(
+    intervals1: &[(usize, usize)],
+    intervals2: &[(usize, usize)],
+) -> Vec<(usize, usize)> {
+    let mut intervals1 = intervals1.to_vec();
+    let mut intervals2 = intervals2.to_vec();
+    intervals1.sort_by_key(|interval| interval.0);
+    intervals2.sort_by_key(|interval| interval.0);
+
+    let mut overlaps = Vec::new();
+    let (mut i, mut j) = (0, 0);
+
+    while i < intervals1.len() && j < intervals2.len() {
+        let (start1, end1) = intervals1[i];
+        let (start2, end2) = intervals2[j];
+
+        if end1 >= start2 && end2 >= start1 {
+            let overlap_start = start1.max(start2);
+            let overlap_end = end1.min(end2);
+            overlaps.push((overlap_start, overlap_end));
+        }
+
+        if end1 < end2 {
+            i += 1;
+        } else {
+            j += 1;
+        }
+    }
+
+    overlaps
+}
+
+fn subtract_intervals(
+    first_intervals: &[(usize, usize)],
+    second_intervals: &[(usize, usize)],
+) -> Vec<(usize, usize)> {
+    let diff = first_intervals
+        .to_vec()
+        .to_interval_set()
+        .difference(&second_intervals.to_vec().to_interval_set());
+    let mut result: Vec<(usize, usize)> = Vec::new();
+    for interval in diff {
+        result.push((interval.lower(), interval.upper()));
+    }
+    result
+}
+#[test]
+fn test_subtract_intervals() {
+  let x: Vec<(usize, usize)> = vec![(1,3), (4, 8), (9,10)];
+  let y: Vec<(usize, usize)> = vec![(2,4), (7,12)];
+  assert_eq!(subtract_intervals(&x, &y), vec![(1,1), (5,6)]);
+}
+
+pub fn highligh_matches<'a>(
+    intervals: &Vec<(Vec<(usize, usize)>, &str)>,
+    input_string: String,
+    overlap_color: &str,
+) -> Line<'a> {
+    let mut intervals: Vec<(Vec<(usize, usize)>, &str)> = format_overlap(intervals, overlap_color);
+    let mut intervals: Vec<(usize, usize, &str)> = intervals
+        .iter()
+        .flat_map(|(vect, color)| {
+            vect.iter()
+                .map(move |&(start, end)| (start, end, color.clone()))
+        })
+        .collect();
+    intervals.sort_by_key(|&(start, _, _)| start);
     let mut result: Vec<Span> = Vec::new();
     let mut current_index = 0;
 
-    for (start, end) in intervals {
+    for (start, end, color) in intervals {
         if current_index < start {
             result.push(input_string[current_index..start].to_string().into());
         }
         if end <= input_string.len() {
             result.push(Span::styled(
                 input_string[start..end].to_string(),
-                Style::new().green(),
+                Style::new().fg(Color::from_str(color).unwrap()),
             ));
         }
         current_index = end;
@@ -44,4 +150,28 @@ pub fn highligh_matches(intervals: Vec<(usize, usize)>, input_string: String) ->
     }
 
     Line::from(result)
+}
+
+fn format_overlap<'a>(
+    intervals: &Vec<(Vec<(usize, usize)>, &'a str)>,
+    overlap_color: &'a str,
+) -> Vec<(Vec<(usize, usize)>, &'a str)> {
+    let intervals: Vec<(Vec<(usize, usize)>, &str)> = intervals
+        .iter()
+        .map(|(vector, color)| (merge_intervals(vector), color.clone()))
+        .collect();
+    let overlapped_intervals: Vec<(usize, usize)> = find_intersections(
+        &intervals
+            .iter()
+            .flat_map(|(tuple, _)| tuple.iter().cloned())
+            .collect::<Vec<(usize, usize)>>(),
+    );
+
+    let mut result: Vec<(Vec<(usize, usize)>, &str)> = Vec::new();
+    for (matches, color) in intervals {
+        result.push((subtract_intervals(&matches, &overlapped_intervals), color));
+    }
+    result.push((overlapped_intervals, overlap_color));
+
+    result
 }
